86ab5949092f845bff218f63c8849f9ac9511a12
Leonid <titov281@yandex.ru>
2022-12-26 19:12:50 +0500

    Fixed create_db_engine

--- init_db.py
+++ init_db.py
@@ -1,10 +1,11 @@
 import asyncio
 
-from app.db import engine, create_tables, create_def_permissions, create_admin
+from app.db import create_db_engine, create_tables, create_def_permissions, create_admin
 
 
 async def async_main():
     try:
+        engine = await create_db_engine()
         await create_tables(engine)
         await create_def_permissions(engine)
         await create_admin(engine)
